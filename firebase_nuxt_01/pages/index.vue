69ee581d938b51bce9b8b9e1d7f83ea434c60c3a
markd-hiyield <mark@hiyield.co.uk>
2023-02-21 18:48:30 +0000

    added tutorial firebase_nuxt_01

---
+++ firebase_nuxt_01/pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input v-model="email" type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input v-model="password" type="password" placeholder="Enter Password" name="psw" required>
+
+      <button @click="createUser(email,password)" >Sign up</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const email = ref('')
+const password = ref('')
+
+const createUser = async (email, password) => {
+  const auth = getAuth();
+  const credentials = await createUserWithEmailAndPassword(auth, email, password)
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  return credentials;
+}
+</script>
+
+
+<style> 
+/* Full-width inputs */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+/* Add a hover effect for buttons */
+button:hover {
+  opacity: 0.8;
+}
+
+/* Add padding and margin to container */
+.container {
+  margin: auto;
+  margin-top: 10%;
+  padding: 16px;
+  width: 50%;
+}
+</style>
